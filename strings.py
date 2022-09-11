28c4efeff736e05305468c6a2ca1440b75b598a4
Raivis <71266644+raivis-rasnacs@users.noreply.github.com>
2022-09-11 21:28:30 +0300

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,56 @@
+"""
+Uzdevumi par darbībām ar tekstu
+"""
+
+# Prasa ievadīt vārdu
+vards = input("Ievadi vārdu: ")
+
+# Vārds ar katru burtu jaunā rindā
+def sadala_pa_burtiem(vards)
+  pass
+
+# Izdrukā vārdu ar retinātiem burtiem
+# Piemērs: A l b e r t s
+def retinati_burti(vards):
+  pass
+
+# Izdrukā vārdu no otra gala
+def no_otra_gala(vards):
+  pass
+
+# Klonē vārdu 2 reizes
+# Piemērs: Alberts Alberts Alberts
+def klone(vards):
+  pass
+
+# Atgriež vārda iniciāli (pirmo burtu) un punktu
+# Piemērs: Alberts -> A.
+def inicialis(vards):
+  pass
+
+# Saskaita burtus vārdā
+def burtu_skaits(vards):
+  pass
+
+# Pārslēdz reģistru
+# Piemērs: Alberts -> aLBERTS
+def registrs(vards):
+  pass
+
+# Atgriež vārdu ar visiem lielajiem burtiem
+def visi_lieli_burti(vards):
+  pass
+
+# Pārbauda, vai vārdā nav ciparu
+# Atgriež True, ja ciparu nav un False, ja ir cipari
+def nav_ciparu(vards):
+  pass
+
+# Pārbauda, vai vārdā ir dotais burts
+# Atgriež True, ja vārdā ir dotais burts un False, ja vārdā nav dotā burtā
+def mekle_burtu(vards, burts):
+  pass
+
+# Atgriež ASCII vērtību pirmajam burtam
+def ascii_vertiba(vards):
+  pass
